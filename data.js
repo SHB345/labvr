7fbf12a7b4133e99f3f63a614df233bb409a9bfa
Shahab Uddin <91548740+SHB345@users.noreply.github.com>
2025-10-20 08:40:55 +0500

    new images added

--- data.js
+++ data.js
@@ -1,45 +1,7 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-panorama-1",
-      "name": "panorama (1)",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "yaw": 0.19936527023474326,
-        "pitch": 0.1310577287470629,
-        "fov": 1.38217411905719
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.039924926698814645,
-          "pitch": 0.01889109509560427,
-          "rotation": 6.283185307179586,
-          "target": "2-panorama"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "1-panorama-2",
+      "id": "0-panorama-2",
       "name": "panorama (2)",
       "levels": [
         {
@@ -68,10 +30,73 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.846971535094319,
-          "pitch": 0.03825386971670852,
+          "yaw": 0.7440790021425663,
+          "pitch": -0.09087623354347407,
           "rotation": 0,
           "target": "2-panorama"
+        },
+        {
+          "yaw": 1.8495377032451161,
+          "pitch": -0.012994535793698248,
+          "rotation": 0,
+          "target": "4-panorama-4"
+        },
+        {
+          "yaw": 3.056435040599572,
+          "pitch": 0.13105932189947822,
+          "rotation": 0,
+          "target": "5-panorama-3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.8512668555579532,
+          "pitch": -0.10119300510906726,
+          "title": "CNC Laser Cutting Machine",
+          "text": "Text"
+        },
+        {
+          "yaw": 3.045617189232245,
+          "pitch": 0.04841551199971761,
+          "title": "Hot Wire Cutting Machine",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-panorama-1",
+      "name": "panorama (1)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.12936362971380078,
+        "pitch": 0.021663839655992945,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.09868823314663189,
+          "pitch": -0.01217898591886879,
+          "rotation": 0,
+          "target": "1-panorama-1"
         }
       ],
       "infoHotspots": []
@@ -106,26 +131,202 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 1.1720780583373216,
-          "pitch": -0.1684630106964189,
+          "yaw": -1.107343954741852,
+          "pitch": -0.014745362740328005,
           "rotation": 0,
-          "target": "0-panorama-1"
+          "target": "0-panorama-2"
         },
         {
-          "yaw": -0.9666621117380281,
-          "pitch": -0.04758237508836771,
+          "yaw": 1.1803986009687417,
+          "pitch": -0.22698862434022082,
           "rotation": 0,
-          "target": "1-panorama-2"
+          "target": "1-panorama-1"
+        },
+        {
+          "yaw": -2.1927469564648163,
+          "pitch": 0.08759036304065226,
+          "rotation": 0,
+          "target": "4-panorama-4"
+        },
+        {
+          "yaw": -1.4925953644835772,
+          "pitch": -0.009901049265359063,
+          "rotation": 0,
+          "target": "5-panorama-3"
         }
       ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1899530230976705,
+          "pitch": 0.0024919840915273994,
+          "title": "CNC Laser Cutting Machine",
+          "text": "<div>Description: CNC laser cutter relies on computer numerical control and computer instructions (G-code) to perform its sequence of cutting operations.</div><div><br></div><div>Specifications:&nbsp;</div><div><br></div><div>X travel. 3000 mm minimum</div><div>&nbsp;Y travel. 1500 mm minimum</div><div>Z travel. 100 mm or better</div><div>Input power&nbsp; &lt; 4KW</div><div>Working Temperature :15-35º C</div><div>Sheet size (to be cut). (2500 x 1500) mm (minimum)</div>"
+        },
+        {
+          "yaw": 1.1795591439307938,
+          "pitch": -0.3390979660872073,
+          "title": "Exit",
+          "text": "Text"
+        },
+        {
+          "yaw": -1.5057344023514325,
+          "pitch": -0.10516396305248499,
+          "title": "Hot Wire Cutting Machine",
+          "text": "<div>Description: A hot wire cutter is used to cut polystyrene foam. The device consists of a thin, taut metal wire, often made of nichrome alloy, which is heated via electrical resistance to approximately 200°C</div><div><br></div><div>Specifications:&nbsp;</div><div>Machine Voltage:110 -240 V</div><div>Temperature :200°C</div>"
+        }
+      ]
+    },
+    {
+      "id": "3-panorama-5",
+      "name": "panorama (5)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
       "infoHotspots": []
+    },
+    {
+      "id": "4-panorama-4",
+      "name": "panorama (4)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 3.120937852728611,
+          "pitch": 0.11536242771391159,
+          "rotation": 0,
+          "target": "5-panorama-3"
+        },
+        {
+          "yaw": -0.2538532374655613,
+          "pitch": 0.263649210908115,
+          "rotation": 0,
+          "target": "2-panorama"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.4339099841623542,
+          "pitch": -0.1477421785889561,
+          "title": "CNC Laser Cutting Machine",
+          "text": "<div>Description: CNC laser cutter relies on computer numerical control and computer instructions (G-code) to perform its sequence of cutting operations.</div><div><br></div><div>Specifications:&nbsp;</div><div><br></div><div>X travel. 3000 mm minimum</div><div>&nbsp;Y travel. 1500 mm minimum</div><div>Z travel. 100 mm or better</div><div>Input power&nbsp; &lt; 4KW</div><div>Working Temperature :15-35º C</div><div>Sheet size (to be cut). (2500 x 1500) mm (minimum)</div>"
+        },
+        {
+          "yaw": 3.1149324322734238,
+          "pitch": 0.029196019342041524,
+          "title": "Hot Wire Cutting Machine",
+          "text": "Text"
+        },
+        {
+          "yaw": -0.24277201533938175,
+          "pitch": 0.17041679231635598,
+          "title": "Back",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "5-panorama-3",
+      "name": "panorama (3)",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.438529422417769,
+          "pitch": 0.020530612225677203,
+          "rotation": 0,
+          "target": "0-panorama-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.091934219759807,
+          "pitch": -0.0059694717544331866,
+          "title": "Hot Wire Cutting Machine",
+          "text": "<div>Description: A hot wire cutter is used to cut polystyrene foam. The device consists of a thin, taut metal wire, often made of nichrome alloy, which is heated via electrical resistance to approximately 200°C</div><div><br></div><div>Specifications:&nbsp;</div><div>Machine Voltage:110 -240 V</div><div>Temperature :200°C</div>"
+        },
+        {
+          "yaw": -2.436353437515953,
+          "pitch": -0.0637603183556017,
+          "title": "Back",
+          "text": "Text"
+        }
+      ]
     }
   ],
-  "name": "AVDL",
+  "name": "Project Title",
   "settings": {
     "mouseViewMode": "drag",
-    "autorotateEnabled": true,
-    "fullscreenButton": true,
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
     "viewControlButtons": true
   }
 };
